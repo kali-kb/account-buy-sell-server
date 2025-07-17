879fc260430dd10c1e2a4e0d7ddb65dd432dabeb
kali <kali@git.com>
2025-07-17 07:06:35 -0700

    update to webhook for telegram bot

--- bot.js
+++ bot.js
@@ -1090,16 +1090,16 @@ bot.action('withdraw_balance', async (ctx) => {
 
 
 // Start the bot
-bot.launch()
-  .then(() => {
-    logger.info('Bot started successfully');
-    if (!MINI_APP_URL) {
-      logger.warn('MINI_APP_URL is not configured in environment variables');
-    }
-  })
-  .catch((err) => {
-    logger.error('Error starting bot', { error: err.message, stack: err.stack });
-  });
+// bot.launch()
+//   .then(() => {
+//     logger.info('Bot started successfully');
+//     if (!MINI_APP_URL) {
+//       logger.warn('MINI_APP_URL is not configured in environment variables');
+//     }
+//   })
+//   .catch((err) => {
+//     logger.error('Error starting bot', { error: err.message, stack: err.stack });
+//   });
 
 // Enable graceful stop
 process.once('SIGINT', () => bot.stop('SIGINT'));
